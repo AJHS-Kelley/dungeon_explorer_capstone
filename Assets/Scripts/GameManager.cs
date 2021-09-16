a72a6611c077d45d9f43c3b6e554f2e99c10464d
Cook <s11098563@students.duvalschools.org>
2021-09-16 08:17:23 -0400

    Initial Commit
    Pushing work done so far, as logged in Capstone Spreadsheet.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private void Awake()
+    {
+        if(GameManager.instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+    }
+
+    // Resources
+    public List<Sprite> playerSprites;
+    public List<Sprite> weaponSprites;
+    public List<int> weaponPrices;
+    public List<int> xpTable;
+
+    // References
+    public Player Player;
+    // public weapon weapon...
+
+    // Logic
+    public int rubies;
+    public int experience;
+
+    // Save state
+    /*
+        int preferedSkin
+        int rubies
+        int experience
+        int weaponLevel
+    */
+    public void SaveState()
+    {
+        string s = "";
+
+        s += "0" + "|";
+        s += rubies.ToString() + "|";
+        s += experience.ToString() + "|";
+        s += "0";
+
+        PlayerPrefs.SetString("SaveState", s);
+    }
+
+    public void LoadState()
+    {
+        if(!PlayerPrefs.HasKey("SaveState"))
+            return;
+
+        string[] data = PlayerPrefs.GetString("SaveState").Split('|');
+
+        // Change player skin
+        rubies = int.Parse(data[1]);
+        experience = int.Parse(data[2]);
+        // Change weapon level
+
+        Debug.Log("LoadState");
+    }
+}
