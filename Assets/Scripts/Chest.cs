7224060a41c39e2df72eff4d7cf007cc4a14afdc
Cook <s11098563@students.duvalschools.org>
2021-09-20 09:31:34 -0400

    v0.55
    Work logged in Capstone Spreadsheet.

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -13,7 +13,7 @@ public class Chest : Collectable
         {
             collected = true;
             GetComponent<SpriteRenderer>().sprite = emptyChest;
-            GameManager.instance.ShowText("+" + rubiesAmount + " Rubies!", 25, Color.red, transform.position, Vector3.up * 25, 1.5f);
+            GameManager.instance.ShowText("+" + rubiesAmount + " Rubies!", 25, Color.yellow, transform.position, Vector3.up * 25, 1.5f);
         }
     }
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,44 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Mover
 {
-    private BoxCollider2D BoxCollider;
-    private Vector3 moveDelta;
-    private RaycastHit2D hit;
-
-    private void Start()
-    {
-        BoxCollider = GetComponent<BoxCollider2D>();
-    }
-
     private void FixedUpdate()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
-        // Reset moveDelta
-        moveDelta = new Vector3(x,y,0);
-
-        // Swap sprite direction based on movement direction.
-        if (moveDelta.x > 0)
-            transform.localScale = Vector3.one;
-        else if (moveDelta.x < 0)
-            transform.localScale = new Vector3(-1, 1, 1);
-
-        // Make sure we can move in this direction by casting a box there first, if box returns null free to move.
-        hit = Physics2D.BoxCast(transform.position, BoxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
-        if (hit.collider == null)
-        {
-            // Movement
-            transform.Translate(0,moveDelta.y * Time.deltaTime, 0);
-        }
-
-        hit = Physics2D.BoxCast(transform.position, BoxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
-        if (hit.collider == null)
-        {
-            // Movement
-            transform.Translate(moveDelta.x * Time.deltaTime,0, 0);
-        }
+        UpdateMotor(new Vector3(x, y, 0));
     }
 }
